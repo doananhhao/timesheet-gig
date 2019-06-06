55383661c05d7069d5fead038d1d621a143a24c2
Nam Tran <namvtran@qasymphony.com>
2019-06-06 19:24:44 +0700

    change jira project

--- templates/js/jira.utils.js
+++ templates/js/jira.utils.js
@@ -7,7 +7,7 @@
         "X-Atlassian-Token": "no-check",
         "Access-Control-Allow-Origin": "*"
       };
-      return fetch(window.jiraUrl + '/rest/api/2/search?jql=assignee=currentuser()%20and%20project=MR%20and%20updated%20>%3D%20-1w&+order+by+updated&fields=summary,worklog,parent,issuetype', {
+      return fetch(window.jiraUrl + '/rest/api/2/search?jql=assignee=currentuser()%20and%20project=AA%20and%20updated%20>%3D%20-1w&+order+by+updated&fields=summary,worklog,parent,issuetype', {
         method: 'GET',
         credentials: 'same-origin',
         headers: header
@@ -26,6 +26,22 @@
         method: 'GET',
         credentials: 'same-origin',
         headers: header
+      }).then(res => {
+        return res.json();
+      });
+    },
+
+    pushJiraWorklog: (worklog, issueKey) => {
+      let header = {
+        "Content-type": "application/json",
+        "X-Atlassian-Token": "no-check",
+        "Access-Control-Allow-Origin": "*"
+      };
+      return fetch(window.jiraUrl + '/rest/api/2/issue/' + issueKey +'/worklog', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: header,
+        body: JSON.stringify({started: worklog.started, timeSpentSeconds: worklog.timeSpentSeconds})
       }).then(res => {
         return res.json();
       });
