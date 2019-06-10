4e01a82f5f5a5fdffbb569a77e901c00a31634c6
Nam Tran <namvtran@qasymphony.com>
2019-06-10 13:44:57 +0700

    fix update jira url and projectKey method

--- templates/index.js
+++ templates/index.js
@@ -5,13 +5,18 @@ const externalFiles = {
   listIssues: "jira/issues/listIssues",
 };
 
-chrome.storage.local.get(['jiraUrl'], function (result) {
+chrome.storage.local.get(['jiraUrl', 'projectKey'], function (result) {
   jiraUrlInput.value = result.jiraUrl;
   window.jiraUrl = result.jiraUrl;
+  window.projectKey = result.projectKey;
+  if (window.jiraUrl && window.projectKey) {
+    loadJiraWorkLogGrid(window.projectKey);
+  }
 });
 
 jiraUrlInput.onkeydown = (event) => {
   if (event.key === "Enter") {
+    chrome.storage.local.set({ 'jiraUrl': jiraUrlInput.value});
     window.jiraUtils.getJiraProjects().then((res) => {
       for (let i = 0; i < res.length; i++) {
         jiraProjectOption.options[i + 1] = new Option(res[i].name, res[i].key);
@@ -22,12 +27,15 @@ jiraUrlInput.onkeydown = (event) => {
 };
 
 jiraProjectOption.onchange = (event) => {
-  chrome.storage.local.set({ 'jiraUrl': jiraUrlInput.value,}, loadJiraWorkLogGrid(event.currentTarget.options[event.currentTarget.selectedIndex].value));
+  loadJiraWorkLogGrid(event.currentTarget.options[event.currentTarget.selectedIndex].value);
 }
 
 loadJiraWorkLogGrid = (projectKey) => {
   let jiraUrlContainer = document.getElementById("jiraUrlContainer");
-  window.projectKey=projectKey;
+  if (window.projectKey == null) {
+    chrome.storage.local.set({ 'projectKey': event.currentTarget.options[event.currentTarget.selectedIndex].value});
+    window.projectKey=projectKey;
+  }
   document.getElementById("container").removeChild(jiraUrlContainer);
   window.jiraUtils.checkLogin().then((res) => {
 		window.userInfo = res;
